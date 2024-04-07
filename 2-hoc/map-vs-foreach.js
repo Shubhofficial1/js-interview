84df667f73a13e252b435ef74147e42cee0a3654
shubham kumar <shubhofficial1@gmail.com>
2024-04-07 17:24:36 +0530

    Added map vs foreach difference

---
+++ 2-hoc/map-vs-foreach.js
@@ -0,0 +1,5 @@
+// Map vs ForEach
+
+// Map is a function in js that return a new array after applying a callback fucntion on each element. It returns a new array. It does not mutate the original array. For eg : arr.map((element)=>element*2)
+
+// Foreach : it is used to execute a fucntion on each element on the given array. It does return undefined. It mutates the original array. arr.forEach((element)=>element*2)
